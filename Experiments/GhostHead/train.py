68b14f19692e382a7a895c077fe37c74ea88b0e3
TT22Brent <tt@tt.com>
2025-05-12 21:18:06 +0800

    Ghosthead test

---
+++ Experiments/GhostHead/train.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0, "./ultralytics")  # 使用当前目录下的本地源码
+
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("ghosthead.yaml")  # 从零构建（也可以用 yolov8n.pt）
+
+    model.train(
+        data="C:/Users/user/PycharmProjects/Yolov8ncloud/Datasets/dataset.yaml",
+        epochs=500,
+        imgsz=640,
+        batch=8,
+        device=0,  # GPU id，或 "cpu"
+        project="ghosthead",
+        name="test1",
+        exist_ok=True
+    )
+
+if __name__ == "__main__":
+    main()
